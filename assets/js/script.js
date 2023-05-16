ce0502a3d71fd7ce922f14907757e4bb0259447b
rafaelpcisterne <rpcsistemasweb@gmail.com>
2023-05-16 10:14:26 -0300

    Added Digital Watch files.

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/**
+ DEV FullStack
+ Rafael P. Cisterne
+ */
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+const clock = setInterval(function time() {
+    let dateToday = new Date()
+    let h = dateToday.getHours()
+    let m = dateToday.getMinutes()
+    let s = dateToday.getSeconds()
+
+    if (h < 10) h = `0${h}`
+    if (m < 10) m = `0${m}`
+    if (s < 10) s = `0${s}`
+
+    hours.textContent = h
+    minutes.textContent = m
+    seconds.textContent = s
+})
